1ae4d871c4a84562b4f030c7674077c6e050ae30
Arjun <arjunprahladan@gmail.com>
2016-06-02 13:15:03 +0530

    Adding comment

--- Enum/src/D.java
+++ Enum/src/D.java
@@ -1,4 +1,5 @@
 // using an enum from inherited interface
+// Enum cannot be created in a constructor
 public class D implements C{
 	public static void main(String[] args) {
 		System.out.println(XYZ.x2);
