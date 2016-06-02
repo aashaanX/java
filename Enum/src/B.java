490341f329a839621e85d88f6ffc72c8b421b8af
Arjun <arjunprahladan@gmail.com>
2016-06-02 13:06:57 +0530

    Enum declaration inside a class

---
+++ Enum/src/B.java
@@ -0,0 +1,12 @@
+// Declaring a enum inside a class.
+// accessing the enum element by declaring an enum variable.
+public class B {
+	enum test{
+		t1,t2,t3;
+	}
+	public static void main(String[] args) {
+		test a = test.t1;
+		System.out.println(a);
+	}
+
+}
